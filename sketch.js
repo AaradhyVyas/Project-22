d435d1211d948ebe273ef8f5a199fcb6013cd1fe
AaradhyVyas <77629845+AaradhyVyas@users.noreply.github.com>
2021-04-29 18:12:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function preload()
 	starImg = loadImage("images/star.png");
 	bgImg = loadImage("images/starNight.png");
 	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	fairyVoice = loadSound("sound/joyMusic.mp3");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
 }
 
 function setup() {
